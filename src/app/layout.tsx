2442c2106987e6dc02a4611ca735f16456fe3067
Morgan Giraud <contact@morgangiraud.com>
2023-05-16 21:40:02 +0200

    Add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 import { GameContextProvider } from "@/GameContext";
 
@@ -18,7 +19,10 @@ export default function RootLayout({
   return (
     <GameContextProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          {children}
+          <Analytics />
+        </body>
       </html>
     </GameContextProvider>
   );
--- src/components/CellView.tsx
+++ src/components/CellView.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 
 import { CellState, Diff, NB_MAX_MOVES, Territory } from "@/constants";
-import { Fort } from "@/svgs/Fort";
+
 import { Cell } from "@/Game";
 import { classNames } from "@/utils";
 import { DashedCircle } from "@/svgs/DashedCircle";
 import { Circle } from "@/svgs/Circle";
+import { Fort } from "@/svgs/Fort";
 
 interface CellViewProps {
   cell: Cell;
